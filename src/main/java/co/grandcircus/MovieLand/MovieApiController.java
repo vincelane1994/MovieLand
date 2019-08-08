d0c075ad73a1aea8f0884cf27484a824b9995d52
vincelane1994 <vincelane1994@gmail.com>
2019-08-08 13:02:17 -0400

    Messed with DAO

--- src/main/java/co/grandcircus/MovieLand/MovieApiController.java
+++ src/main/java/co/grandcircus/MovieLand/MovieApiController.java
@@ -1,8 +1,10 @@
 package co.grandcircus.movieland;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.grandcircus.movieland.dao.MovieRepository;
@@ -20,26 +22,30 @@ public class MovieApiController {
 //	}
 //	
 //	GetMapping filterable by; find all, length, genre and rating.
-//	@GetMapping("/movies")
-//	public List<Movie> listMovies(
-//			@RequestParam(value="length", required=false)Integer length,
-//			@RequestParam(value="genre", required=false)String genre,
-//			@RequestParam(value="rating", required=false)Double rating){
-//		
-//	}
-	
+	@GetMapping("/movies")
+	public List<Movie> listMovies(@RequestParam(value = "length", required = false) Integer length,
+			@RequestParam(value = "genre", required = false) String genre,
+			@RequestParam(value = "rating", required = false) Double rating) {
+		if ((length == null || length.isEmpty()) && (genre == null || genre.isEmpty())) {
+			return dao.findAll();
+		} else if (length == null || length.isEmpty()) {
+			return dao.findByGenreContainsIgnoreCase(genre);
+		} else
+			return dao.findByLength(length);
+	}
+
 //	GetMapping for the watchlist
 //	@GetMapping("/watchlist")
 //	public List<Movie> showWatchlist(){
 //		
 //	}
-	
+
 //	GetMapping to display details of an individual movie
 //  @GetMapping("/details")
 //	public Movie showMovieDetail(){
 //	
 //  }
-	
+
 //	PutMapping for adding to watchlist
 //	@PutMapping("/watchlist-add")
 //	public Movie addToWatchlist() {
@@ -51,6 +57,5 @@ public class MovieApiController {
 //	public Movie removeFromWatchlist() {
 //		
 //	}
-	
-	
+
 }
--- src/main/java/co/grandcircus/MovieLand/dao/MovieRepository.java
+++ src/main/java/co/grandcircus/MovieLand/dao/MovieRepository.java
@@ -1,6 +1,8 @@
 package co.grandcircus.movieland.dao;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import co.grandcircus.movieland.entities.Movie;
@@ -9,5 +11,7 @@ import co.grandcircus.movieland.entities.Movie;
 
 
 public interface MovieRepository extends JpaRepository<Movie, Long>{
-
+	List<Movie> findByGenreContainsIgnoreCase(String genre);
+	List<Movie> findByLength(Integer length);
+	
 }
